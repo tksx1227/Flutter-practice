21902b7c163024a0e17c23700aab2a2dab76cc8d
tomo-taku <tmrtkm1227tksx@gmail.com>
2021-07-23 23:51:42 +0900

    ~10:44

--- flutter_design_copy/lib/screens/home/components/body.dart
+++ flutter_design_copy/lib/screens/home/components/body.dart
@@ -1,61 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_design_copy/constants.dart';
+import 'package:flutter_design_copy/screens/home/components/header_with_searchbox.dart';
+import 'package:flutter_design_copy/screens/home/components/title_with_more_btn.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // It will provite us total height and width of our screen
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: <Widget>[
-        Container(
-          // It will cover 20% of our total height
-          height: size.height * 0.2,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                height: size.height * 0.2 - 27,
-                decoration: BoxDecoration(
-                  color: kPrimaryColor,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(36),
-                    bottomRight: Radius.circular(36),
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  height: 54,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 50,
-                        color: kPrimaryColor.withOpacity(0.23),
-                      ),
-                    ],
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Search",
-                      hintStyle: TextStyle(
-                        color: kPrimaryColor.withOpacity(0.5),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
+    // It enable scrolling on small device
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          HeaderWithSearchBox(size: size),
+          TitleWithMoreBtn(
+            title: "Recomended",
+            press: () {},
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
