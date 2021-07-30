9dbcb94c3baa59f0a0aa13a3f9644402dd173a41
tomo-taku <tmrtkm1227tksx@gmail.com>
2021-07-30 12:13:59 +0900

    Done

--- flutter_design_copy_2/lib/screen/messages/components/body.dart
+++ flutter_design_copy_2/lib/screen/messages/components/body.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_design_copy_2/constants.dart';
+import 'package:flutter_design_copy_2/models/ChatMessage.dart';
+import 'package:flutter_design_copy_2/screen/messages/components/message.dart';
+import 'package:flutter_design_copy_2/screen/messages/components/chat_input_field.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Spacer(),
-        Container(
-          padding: EdgeInsets.symmetric(
-            horizontal: kDefaultPadding,
-            vertical: kDefaultPadding / 2,
-          ),
-          decoration: BoxDecoration(
-            color: Colors.blue,
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            child: ListView.builder(
+              itemCount: demeChatMessages.length,
+              itemBuilder: (context, index) => Message(
+                message: demeChatMessages[index],
+              ),
+            ),
           ),
         ),
+        ChatInputField(),
       ],
     );
   }
