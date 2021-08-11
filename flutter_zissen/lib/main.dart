07d3027c9205446fbf055f13d65de76b0f222f3e
tomo-taku <tmrtkm1227tksx@gmail.com>
2021-08-11 18:39:15 +0900

    Firestoreとの連携

--- flutter_zissen/lib/main.dart
+++ flutter_zissen/lib/main.dart
@@ -1,77 +1,88 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyTodoApp());
+bool USE_FIRESTORE_EMULATOR = false;
 
-class MyTodoApp extends StatelessWidget {
-  const MyTodoApp({Key? key}) : super(key: key);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  if (USE_FIRESTORE_EMULATOR) {
+    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "My Todo App",
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-      ),
-      home: TodoListPage(),
+      debugShowCheckedModeBanner: false,
+      title: "Book list",
+      home: BookListPage(),
     );
   }
 }
 
-class TodoListPage extends StatefulWidget {
-  @override
-  _TodoListPageState createState() => _TodoListPageState();
-}
-
-class _TodoListPageState extends State<TodoListPage> {
-  List<String> todoList = [];
+class BookListPage extends StatelessWidget {
+  final Stream<QuerySnapshot> _usersStream =
+      FirebaseFirestore.instance.collection('books').snapshots();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("リスト一覧"),
+        title: Text("Book List"),
       ),
-      body: ListView.builder(
-        itemCount: todoList.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              title: Text(todoList[index]),
-            ),
-          );
-        },
+      body: Center(
+        child: StreamBuilder<QuerySnapshot>(
+            stream: _usersStream, builder: readData),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final newListText = await Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) {
-            return TodoAddPage();
-          }));
-          if (newListText != null) {
-            setState(() {
-              todoList.add(newListText);
-            });
-          }
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute<void>(
+              builder: (BuildContext context) => BookAddPage(),
+            ),
+          );
         },
         child: Icon(Icons.add),
       ),
     );
   }
+
+  Widget readData(BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+    return ListView(
+      children: snapshot.data!.docs.map((DocumentSnapshot document) {
+        Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+        return Card(
+          child: ListTile(
+            title: Text(data["title"]),
+            subtitle: Text(data["author"]),
+          ),
+        );
+      }).toList(),
+    );
+  }
 }
 
-class TodoAddPage extends StatefulWidget {
+class BookAddPage extends StatefulWidget {
   @override
-  _TodoAddPageState createState() => _TodoAddPageState();
+  _BookAddPageState createState() => _BookAddPageState();
 }
 
-class _TodoAddPageState extends State<TodoAddPage> {
-  String _text = "";
+class _BookAddPageState extends State<BookAddPage> {
+  CollectionReference _colRef = FirebaseFirestore.instance.collection('books');
+  String _title = "";
+  String _author = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("リスト追加"),
+        title: Text("本を追加"),
       ),
       body: Container(
         padding: EdgeInsets.all(64),
@@ -79,14 +90,26 @@ class _TodoAddPageState extends State<TodoAddPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              _text,
+              "タイトル：$_title",
+              style: TextStyle(color: Colors.blue),
+            ),
+            Text(
+              "著者：$_author",
               style: TextStyle(color: Colors.blue),
             ),
             const SizedBox(height: 8),
             TextField(
               onChanged: (String value) {
                 setState(() {
-                  _text = value;
+                  _title = value;
+                });
+              },
+            ),
+            const SizedBox(height: 8),
+            TextField(
+              onChanged: (String value) {
+                setState(() {
+                  _author = value;
                 });
               },
             ),
@@ -95,10 +118,20 @@ class _TodoAddPageState extends State<TodoAddPage> {
               width: double.infinity,
               child: ElevatedButton(
                 onPressed: () {
-                  Navigator.of(context).pop(_text);
+                  _colRef
+                      .add({"title": _title, "author": _author}).then((value) {
+                    print("Books Added");
+                    setState(() {
+                      _title = "";
+                      _author = "";
+                    });
+                    Navigator.of(context).pop();
+                  }).catchError((error) {
+                    print("Failed to add books: $error");
+                  });
                 },
                 child: Text(
-                  "リスト追加",
+                  "追加",
                   style: TextStyle(
                     color: Colors.white,
                   ),
